15cedbfb3d30672e3e5ce91bb55bb71138d41a58
Wesley Krug <Wesley Krug>
2017-04-14 19:23:49 -0400

    Made a treadmill

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,15 @@
 var scene, camera, renderer;
 var geometry, material;
 var treeCF, treeTrans, treeScale, treeRot;
-
-var ray, brush, objectHovered,
-    mouse3D, isMouseDown = false, onMouseDownPosition,
-    radious = 1600, onMouseDownTheta = 45, onMouseDownPhi = 60,
-    isShiftDown = false;
+var groundTrans, groundRot, groundScale;
 var mouse = [.5, .5];
 var objects = {};
 var phi = 0, theta = 0;
 var target = new THREE.Vector3();
 var trees = [];
 var treesCF = [];
+var grounds = [];
+var groundsCF = [];
 const TREESPEED = -25;
 const NUMTREES = 20;
 
@@ -19,12 +17,9 @@ target.x = 10 * Math.sin( phi ) * Math.cos( theta );
 target.y = 10 * Math.cos( phi );
 target.z = 10 * Math.sin( phi ) * Math.sin( theta );
 
-
 init();
 animate();
-
 function init() {
-
     scene = new THREE.Scene();
     window.addEventListener('resize', onResize, false);
     window.addEventListener('keydown', onKeypress, false);
@@ -33,15 +28,6 @@ function init() {
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position = new THREE.Vector3(0, 0, 0);
     camera.target = target;
-    // const eyePos = new THREE.Vector3 (1000, 300, 400);
-    // const cameraPose = new THREE.Matrix4().lookAt (
-    //     eyePos,
-    //     new THREE.Vector3 (0, 0, 200),
-    //     new THREE.Vector3 (0, 0, 1)
-    // );
-    // cameraPose.setPosition (eyePos);
-    // camera.matrixAutoUpdate = false;
-    // camera.matrixWorld.copy (cameraPose);
     window.addEventListener( 'mousemove', onMouseMove, false );
     for(i = 0; i < NUMTREES; i++) {
         let temp = new Tree();
@@ -55,41 +41,33 @@ function init() {
         treesCF.push(tempCF);
         scene.add(temp);
     }
-
     treeTrans = new THREE.Vector3();
     treeScale = new THREE.Vector3();
     treeRot = new THREE.Quaternion();
 
-    //treeCF.multiply(new THREE.Matrix4().makeRotationX(1.5));
-    //treeCF.multiply(new THREE.Matrix4().makeTranslation(-1000,0,10000));
     const lightOne = new THREE.DirectionalLight(0xFFFFFF);
     lightOne.position.set(0,1,-1).normalize();
     scene.add(lightOne);
 
-
-
-    scene.add( trees );
-
-    const groundTex = new THREE.ImageUtils.loadTexture("Images/leaves12.jpg");
+    const groundTex = new THREE.ImageUtils.loadTexture("Images/grass.jpg");
     groundTex.repeat.set(3.8,6);
     groundTex.wrapS = THREE.RepeatWrapping;
     groundTex.wrapT = THREE.RepeatWrapping;
     const groundMat = new THREE.MeshPhongMaterial({ map: groundTex});
 
     const groundGeo = new THREE.BoxGeometry(3000,10,10000, 10,10,10);
-    const ground = new THREE.Mesh (groundGeo, groundMat);
-    ground.position = new THREE.Vector3(0,1000,-1000);
-    let groundCF = new THREE.Matrix4();
-    groundCF.multiply(new THREE.Matrix4().makeTranslation(0,-250,5000));
-    var groundTrans = new THREE.Vector3();
-    var groundRot = new THREE.Quaternion();
-    var groundScale = new THREE.Vector3();
-
-    groundCF.decompose(groundTrans, groundRot, groundScale);
-    ground.position.copy(groundTrans);
-    ground.quaternion.copy(groundRot);
-    ground.scale.copy(groundScale);
-    scene.add(ground);
+    for(i = 0; i < 3; i++) {
+        var ground = new THREE.Mesh (groundGeo, groundMat);
+        //ground.position = new THREE.Vector3(0,1000,-1000*i);
+        let groundCF = new THREE.Matrix4();
+        groundCF.multiply(new THREE.Matrix4().makeTranslation(0,-300,10000*i));
+        grounds.push(ground);
+        groundsCF.push(groundCF);
+        scene.add(ground);
+    }
+    groundTrans = new THREE.Vector3();
+    groundRot = new THREE.Quaternion();
+    groundScale = new THREE.Vector3();
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -115,20 +93,24 @@ function animate() {
             treesCF[i].multiply(new THREE.Matrix4().makeTranslation(t,0,10000-i*100));
         }
         treesCF[i].multiply(new THREE.Matrix4().makeTranslation(0,0,TREESPEED));
-
         treesCF[i].decompose (treeTrans, treeRot, treeScale);  // decompose the coord frame
-
         trees[i].position.copy(treeTrans);   /* apply the transformation */
         trees[i].quaternion.copy(treeRot);
         trees[i].scale.copy (treeScale);
+        if (i >= 0 && i < 3) {
+            if (grounds[i].position.z < -5000) {
+                groundsCF[i] = new THREE.Matrix4();
+                groundsCF[i].multiply(new THREE.Matrix4().makeTranslation(0,-300,10000));
+            }
+            groundsCF[i].multiply(new THREE.Matrix4().makeTranslation(0,0,TREESPEED));
+            groundsCF[i].decompose(groundTrans, groundRot, groundScale);
+            grounds[i].position.copy(groundTrans);
+            grounds[i].quaternion.copy(groundRot);
+            grounds[i].scale.copy(groundScale);
+        }
     }
-
-
-
-
     //mesh.rotation.x += 0.01;
     //mesh.rotation.y += 0.02;
-
     renderer.render( scene, camera );
 }
 
@@ -147,8 +129,8 @@ const rotYpos = new THREE.Matrix4().makeRotationY (THREE.Math.degToRad(5));
 const rotYneg = new THREE.Matrix4().makeRotationY (THREE.Math.degToRad(-5));
 
 function onMouseMove(ev) {
-    mouse[0] = ev.clientX / window.innerWidth;
-    mouse[1] = ev.clientY / window.innerHeight;
+    mouse[0] = -ev.clientX / -window.innerWidth;
+    mouse[1] = -ev.clientY / -window.innerHeight;
 }
 
 function onKeypress(event) {
